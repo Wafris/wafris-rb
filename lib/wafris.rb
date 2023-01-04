949054bc53b93789d8b3bf2c6524e50dba7b1729
Ryan Castillo <rmcastil@gmail.com>
2023-01-04 18:20:52 -0500

    When processing requests load core the first time around

--- lib/wafris.rb
+++ lib/wafris.rb
@@ -15,5 +15,32 @@ module Wafris
     def configuration
       @configuration ||= Wafris::Configuration.new
     end
+
+    # ip: the IP of the client making the request, may be from x-forwarded-for
+    # user_agent: full user agent making the request
+    # path: path including parameters of the request
+    # host: host (website/domain) making the request
+    # time: UTC time of the request (from the logs to match things up)
+
+    def allow_request?(request)
+      configuration.connection_pool.with do |conn|
+        time = Time.now
+        status = conn.evalsha(
+          configuration.script_sha,
+          argv: [
+            request.ip,
+            IPAddr.new(request.ip).to_i,
+            time.to_i,
+            "all-ips:#{time.strftime('%Y-%m-%d')}:#{time.hour}"
+          ]
+        )
+
+        if status.eql? 'Blocked'
+          return false
+        else
+          return true
+        end
+      end
+    end
   end
 end
