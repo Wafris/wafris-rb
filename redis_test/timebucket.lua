0ff719a1a13c62631b1ca4fa36312ce75f072d07
Ryan Castillo <rmcastil@gmail.com>
2023-03-18 13:12:04 -0400

    Refactor for clarity

--- redis_test/timebucket.lua
+++ redis_test/timebucket.lua
@@ -1,15 +1,43 @@
-local function get_timebucket_from_timestamp(unix_time)
-  local day_count, year, days, month = function(yr) return (yr % 4 == 0 and (yr % 100 ~= 0 or yr % 400 == 0)) and 366 or 365 end, 1970, math.ceil(unix_time/86400)
-
-  while days >= day_count(year) do
-      days = days - day_count(year) year = year + 1
+-- Code was pulled from https://otland.net/threads/how-convert-timestamp-to-date-type.251657/
+-- An alternate solution is https://gist.github.com/markuman/e96d04139cd8acc33604
+local function get_timebucket_from_timestamp(unix_time_milliseconds)
+  local function calculate_years_number_of_days(yr)
+    return (yr % 4 == 0 and (yr % 100 ~= 0 or yr % 400 == 0)) and 366 or 365
   end
-  local tab_overflow = function(seed, table) for i = 1, #table do if seed - table[i] <= 0 then return i, seed end seed = seed - table[i] end end
-  month, days = tab_overflow(days, {31,(day_count(year) == 366 and 29 or 28),31,30,31,30,31,31,30,31,30,31})
-  local hours, minutes, seconds = math.floor(unix_time / 3600 % 24), math.floor(unix_time / 60 % 60), math.floor(unix_time % 60)
-  local period = hours > 12 and "pm" or "am"
+
+  local function get_year_and_day_number(year, days)
+    while days >= calculate_years_number_of_days(year) do
+      days = days - calculate_years_number_of_days(year)
+      year = year + 1
+    end
+    return year, days
+  end
+
+  local function get_month_and_month_day(days, year)
+    local days_in_each_month = {
+      31,
+      (calculate_years_number_of_days(year) == 366 and 29 or 28),
+      31, 30, 31,30,31,31,30,31,30,31
+    }
+
+    for month = 1, #days_in_each_month do
+      if days - days_in_each_month[month] <= 0 then return month, days end
+      days = days - days_in_each_month[month]
+    end
+  end
+
+  local unix_time = unix_time_milliseconds / 1000
+  local year = 1970
+  local days = math.ceil(unix_time/86400)
+  local month = nil
+
+  year, days = get_year_and_day_number(year, days)
+  month, days = get_month_and_month_day(days, year)
+  local hours = math.floor(unix_time / 3600 % 24)
+  -- local minutes, seconds = math.floor(unix_time / 60 % 60), math.floor(unix_time % 60)
   -- hours = hours > 12 and hours - 12 or hours == 0 and 12 or hours
-  return string.format("%04d-%02d-%02d-%02d", year, month, days, hours, minutes, seconds, period)
+  return string.format("%04d-%02d-%02d-%02d", year, month, days, hours)
 end
 
-return get_timebucket_from_timestamp('1678039768')
+get_timebucket_from_timestamp('1678991574677')
+redis.debug(get_timebucket_from_timestamp('1678991574677')
