8d999e59e650485d59b392ef5caab4a4c38d99f7
Ryan Castillo <rmcastil@gmail.com>
2023-03-23 19:04:12 -0400

    Build out directory structure for fixtures

--- test/fixtures/ip_data_generator.rb
+++ test/fixtures/ip_data_generator.rb
@@ -1,18 +1,18 @@
+# frozen_string_literal: true
+
 require 'faker'
 require 'ipaddr'
-
+require 'debug'
 
 # Generates a timestamp in the last 24 hours of elapsed time
-end_time = Time.now.to_i
-start_time = (end_time - (24*60*60)).to_i
+end_time = (Time.now.to_f * 1000).to_i
+start_time = (end_time - (24*60*60*1000)).to_i
 
 if ARGV[0]
   number_to_generate = ARGV[0].to_i
 else
-  number_to_generate = 1000
+  number_to_generate = 10
 end
-
-output_data = ""
 
 ip_list = []
 
@@ -29,9 +29,15 @@ puts "#{ip_list.size} IP addresses generated"
 lua_loader_file = File.open("data_load.lua").read
 lua_loader_file.gsub!("IP_LIST", ip_list.join(","))
 
-# Note: timestamps need to be passed in in ascending order or else the stream adding fails as IDs must be sequentially larger
+# Note: timestamps need to be passed in in ascending order or else the stream adding
+# fails as IDs must be sequentially larger
 lua_loader_file.gsub!("TIMESTAMP_LIST", timestamp_list.sort.join(","))
 
 File.write("#{ARGV[0]}_data_load.lua", lua_loader_file)
 
 return "done"
+
+# Multiple test cases
+# 1. 5 IPs with 5 timestamps each
+# 2. 5 IPs with overlapping timestamps
+#    If the IPs overlap use the Redis unix timestamp
