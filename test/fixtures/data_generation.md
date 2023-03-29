5538b514c5b6e06276b7a8ee4f0558e837fed302
Ryan Castillo <rmcastil@gmail.com>
2023-03-29 11:17:30 -0400

    Generate 10 Ips with 10,000 timestamps

--- test/fixtures/data_generation.md
+++ test/fixtures/data_generation.md
@@ -2,13 +2,7 @@
 
 ## Context
 
-This directory holds utilties for testing the speed and storage use of different Redis configurations.
+This directory holds utilties for generating test data.
 
-## 1. Generating a new lua Data file with Faker
-Call `bundle exec ruby ip_data_generator.rb INTEGER` to generate a new file of ip_addresses and timestamps
-
-## 2. Execute the generated Lua script in Redis
-`redis-cli --eval ./INTEGER_data_load.lua`
-
-## 3. Query Suite
-`TBD`
+## Generating a new lua Data file with Faker
+Call `bundle exec ruby ip_data_generator.rb <INTEGER>` to generate a new file of ip_addresses. <INTEGER> specifies how many IPs we want to generate.
--- test/fixtures/ip_data_generator.rb
+++ test/fixtures/ip_data_generator.rb
@@ -6,36 +6,36 @@ require 'debug'
 
 # Generates a timestamp in the last 24 hours of elapsed time
 end_time = (Time.now.to_f * 1000).to_i
-start_time = (end_time - (24*60*60*1000)).to_i
+start_time = (end_time - (24 * 60 * 60 * 1000)).to_i
 
-if ARGV[0]
-  number_to_generate = ARGV[0].to_i
-else
-  number_to_generate = 10
-end
+number_to_generate = ARGV[0] ? ARGV[0].to_i : 10
 
 ip_list = []
 
-(1..number_to_generate).each do |i|
+(1..number_to_generate).each do
   ip_list << IPAddr.new(Faker::Internet.ip_v4_address).to_i
 end
 
 # Unique list of timestamps in ascending order
-timestamp_list = (start_time..end_time).to_a.shuffle.take(number_to_generate).sort
+timestamp_list = Array.new(10000) { rand(start_time..end_time) }.sort
 
 puts "#{timestamp_list.size} timestamps generated"
 puts "#{ip_list.size} IP addresses generated"
 
-lua_loader_file = File.open("data_load.lua").read
+lua_loader_file = File.open(
+  File.join(File.dirname(__FILE__), "./ips_and_timestamps.lua.template")
+).read
 lua_loader_file.gsub!("IP_LIST", ip_list.join(","))
 
-# Note: timestamps need to be passed in in ascending order or else the stream adding
-# fails as IDs must be sequentially larger
+# # Note: timestamps need to be passed in in ascending order or else the stream adding
+# # fails as IDs must be sequentially larger
 lua_loader_file.gsub!("TIMESTAMP_LIST", timestamp_list.sort.join(","))
 
-File.write("#{ARGV[0]}_data_load.lua", lua_loader_file)
-
-return "done"
+File.write(
+  File.join(File.dirname(__FILE__), "ips_and_timestamps.lua"),
+  lua_loader_file
+)
+# return "done"
 
 # Multiple test cases
 # 1. 5 IPs with 5 timestamps each
