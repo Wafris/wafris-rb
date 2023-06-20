0f415df5970d8236748f0972446516f13ce37318
Ryan Castillo <rmcastil@gmail.com>
2023-06-20 09:49:31 -0400

    feat: Introduce ability for user to define their proxies so they won't be considered the request ip

--- lib/wafris/middleware.rb
+++ lib/wafris/middleware.rb
@@ -7,6 +7,23 @@ module Wafris
     end
 
     def call(env)
+      user_defined_proxies = ENV['MY_PROXIES'].split(',') if ENV['MY_PROXIES']
+
+      valid_ipv4_octet = /\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])/
+
+      trusted_proxies = Regexp.union(
+        /\A127#{valid_ipv4_octet}{3}\z/,                          # localhost IPv4 range 127.x.x.x, per RFC-3330
+        /\A::1\z/,                                                # localhost IPv6 ::1
+        /\Af[cd][0-9a-f]{2}(?::[0-9a-f]{0,4}){0,7}\z/i,           # private IPv6 range fc00 .. fdff
+        /\A10#{valid_ipv4_octet}{3}\z/,                           # private IPv4 range 10.x.x.x
+        /\A172\.(1[6-9]|2[0-9]|3[01])#{valid_ipv4_octet}{2}\z/,   # private IPv4 range 172.16.0.0 .. 172.31.255.255
+        /\A192\.168#{valid_ipv4_octet}{2}\z/,                     # private IPv4 range 192.168.x.x
+        /\Alocalhost\z|\Aunix(\z|:)/i,                            # localhost hostname, and unix domain sockets
+        *user_defined_proxies
+      )
+
+      Rack::Request.ip_filter = lambda { |ip| trusted_proxies.match?(ip) }
+
       request = Rack::Request.new(env)
 
       if Wafris.configuration.enabled? && Wafris.allow_request?(request)
